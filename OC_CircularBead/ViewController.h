6fbacf45f57ab5bd517597b1313871eee93a31ae
Ashimar <996606326@qq.com>
2017-03-16 15:30:36 +0800

    Initial Commit

---
+++ OC_CircularBead/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OC_CircularBead
+//
+//  Created by myios on 2017/3/16.
+//  Copyright © 2017年 Ashimar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OC_CircularBead/ViewController.m
@@ -0,0 +1,109 @@
+//
+//  ViewController.m
+//  OC_CircularBead
+//
+//  Created by myios on 2017/3/16.
+//  Copyright © 2017年 Ashimar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *myTableView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview:self.myTableView];
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
+
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+//    cell.imageView.image = [UIImage imageNamed:@"1"];
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld",indexPath.row];
+    cell.detailTextLabel.text = @"哎呦还不错哦";
+    
+//    第一种方法:通过设置layer的属性
+    /* */
+//    UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(100, 0, 40, 40)];
+//        imageView.image = [UIImage imageNamed:@"1"];
+//    imageView.backgroundColor = [UIColor orangeColor];
+//    imageView.layer.cornerRadius = 20;
+//    imageView.layer.masksToBounds = YES;
+//    [cell addSubview:imageView];
+    
+    
+//    第二种方法:使用贝塞尔曲线UIBezierPath和Core Graphics框架画出一个圆角
+    UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(100, 0, 40, 40)];
+    imageView.image = [UIImage imageNamed:@"1"];
+    
+    [cell addSubview:imageView];
+    
+    dispatch_async(dispatch_get_global_queue(0,0), ^{
+        //开始对imageView进行画图
+        UIGraphicsBeginImageContextWithOptions(imageView.bounds.size, NO, [UIScreen mainScreen].scale);
+        //使用贝塞尔曲线画出一个圆形图
+        [[UIBezierPath bezierPathWithRoundedRect:imageView.bounds cornerRadius:imageView.frame.size.width] addClip];
+        [imageView drawRect:imageView.bounds];
+        
+        UIImage * image = UIGraphicsGetImageFromCurrentImageContext();
+        //结束画图
+        UIGraphicsEndImageContext();
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            imageView.image = image;
+        });
+        
+    });
+    
+    
+//    第三种方法:使用CAShapeLayer和UIBezierPath设置圆角
+//    UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(100, 0, 40, 40)];
+//    imageView.image = [UIImage imageNamed:@"1"];
+//    UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:imageView.bounds byRoundingCorners:UIRectCornerAllCorners cornerRadii:imageView.bounds.size];
+//    
+//    CAShapeLayer *maskLayer = [[CAShapeLayer alloc]init];
+//    //设置大小
+//    maskLayer.frame = imageView.bounds;
+//    //设置图形样子
+//    maskLayer.path = maskPath.CGPath;
+//    imageView.layer.mask = maskLayer;
+//    [cell addSubview:imageView];
+    
+    return cell;
+}
+
+#pragma mark - lazy load
+
+- (UITableView *)myTableView {
+    if (!_myTableView) {
+        _myTableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain  ];
+        _myTableView.delegate =self;
+        _myTableView.dataSource = self;
+        [_myTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    }
+    return _myTableView;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
